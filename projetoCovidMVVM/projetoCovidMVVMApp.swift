b7f528f9cd26955b9b54c8a2561658ebd305ddca
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2023-02-11 10:59:54 -0300

    Initial Commit

---
+++ projetoCovidMVVM/projetoCovidMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoCovidMVVMApp.swift
+//  projetoCovidMVVM
+//
+//  Created by Roberto Edgar Geiss on 11/02/23.
+//
+
+import SwiftUI
+
+@main
+struct projetoCovidMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
