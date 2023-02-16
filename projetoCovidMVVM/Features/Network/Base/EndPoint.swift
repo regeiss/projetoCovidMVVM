234887651c476f812042e8c906ac2e90f6fbdbc8
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-02-16 17:24:10 -0300

    Update EndPoint.swift

--- projetoCovidMVVM/Features/Network/Base/EndPoint.swift
+++ projetoCovidMVVM/Features/Network/Base/EndPoint.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol Endpoint 
+{
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+}
+
+extension Endpoint 
+{
+    var scheme: String 
+    {
+        return "https"
+    }
+
+    var host: String 
+    {
+        return "disease.sh/v3/covid-19"
+    }
+}
