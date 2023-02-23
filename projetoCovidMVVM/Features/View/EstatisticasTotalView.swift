853200bc3b91cf5730d397a50d1ae29e7319c49f
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-02-23 16:48:34 -0300

    Update EstatisticasTotalView.swift

--- projetoCovidMVVM/Features/View/EstatisticasTotalView.swift
+++ projetoCovidMVVM/Features/View/EstatisticasTotalView.swift
@@ -13,31 +13,49 @@ struct EstatisticasTotalView: View
     
     var body: some View
     {
-        NavigationView
+        VStack()
         {
-            Group
+            HStack
             {
-                switch vm.state
-                {
+                Image(systemName: "line.horizontal.3").foregroundColor(.blue)
+                    .imageScale(.large)
+                    .padding([.leading])
+                
+                Text("COVID-19").foregroundColor(.blue).font(.system(.title3, design: .rounded))
+                Spacer()
+                Text("N/A")
+                Text("N/S")
+                Spacer()
+                Image(systemName: "car.2").foregroundColor(.blue)
+                    .imageScale(.large)
+                    .padding([.trailing])
+            }
+        }
+
+        Group
+        {
+            switch vm.state
+            {
                 case .loading:
-                    LoadingView(text: "Fetching")
+                    LoadingView(text: "Buscando")
                     
                 case .success(let data):
-                    
                     VStack
                     {
                         Text(String(data.population.numberFormat()))
-                        Text(String(data.updated))
-                        Text(String(data.cases).toQuilometrosFormat())
+                        Text(String(data.updated.getDateFromTimeStamp))
+                        Text(String(data.cases.numberFormat()))
                         
                     }
-                default: LoadingView(text: "Fetching1")
-                }
-            }.task { await vm.getAllEstatisticas() }
-                .alert("Error", isPresented: $vm.hasError, presenting: vm.state) { detail in Button("Retry", role: .destructive)
-                                    { Task {await vm.getAllEstatisticas()}}}
-                                message: { detail in if case let .failed(error) = detail { Text(error.localizedDescription)}}
-        }
+
+                default: EmptyView()
+            }
+        }.task { await vm.getAllEstatisticas() }
+        .alert("Error", isPresented: $vm.hasError, presenting: vm.state) { detail in Button("Retry", role: .destructive)
+            { Task {await vm.getAllEstatisticas()}}}
+                message: { detail in if case let .failed(error) = detail { Text(error.localizedDescription)}}
     }
+ 
 }
+
 
