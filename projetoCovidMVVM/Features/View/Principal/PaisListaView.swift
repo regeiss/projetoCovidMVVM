27ba608a834e6487776690ce4128fc6400a217ef
Roberto Edgar Geiss <roberto.geiss.nh@gmail.com>
2023-02-28 22:15:27 -0300

    ajustes foreach

--- projetoCovidMVVM/Features/View/Principal/PaisListaView.swift
+++ projetoCovidMVVM/Features/View/Principal/PaisListaView.swift
@@ -26,60 +26,56 @@ struct PaisListaView: View
         {
             VStack()
             {
-                BarraBuscaView(searchText: $searchText, isSearching: $isSearching)
-                    .padding(.vertical, 5)
-                Divider()
-                    .frame(height: 4)
-                    .background(Color(.systemGray2))
-                    .padding(.bottom, 5)
-                Section(header:
-                            HStack
-                        {
-                    CabecalhoView(title: "País",
-                                  fontSize: 22,
-                                  fontWeight: .bold)
-                    .frame(width: 130, alignment: .leading)
-                    Spacer()
-                    CabecalhoView(title: "Casos",
-                                  fontSize: 22,
-                                  fontWeight: .bold)
-                    .frame(width: 90, alignment: .trailing)
-                    CabecalhoView(title: "Mortes",
-                                  fontSize: 22,
-                                  fontWeight: .bold)
-                    .frame(width: 90, alignment: .trailing)
-                }
-                )
-                // MARK TODO: ajustar foreach
+                let lista = vm.paises
+                List
                 {
-                    let lista = [vm.paises]
-                    List
-                    {
-                        ForEach(searchText.isEmpty ? lista: lista.filter
-                                {
-                            $0.lista.country.lowercased().contains(searchText.lowercased())
-                        }, id: \.country) { country in
-                            LinhaDetalheView(textOne: country.country,
-                                             textTwo: "\(country.cases.numberFormat())",
-                                             textThree: "\(country.deaths.numberFormat())",
-                                             fontSize: 18,
-                                             fontWeight: .medium,
-                                             frameWidth: 140)
-                            .foregroundColor(Color(.secondaryLabel))
-                            //.onTapGesture { vm.selectedCountry = country }
-                        }
-                        //.listRowInsets(EdgeInsets())
+                    ForEach (lista, id: \.countryInfo._id)
+                    { pais in
+//                        LinhaDetalheView(textOne: String(pais.country),
+//                                         textTwo: "\(pais.cases.numberFormat())",
+//                                         textThree: "\(pais.deaths.numberFormat())",
+//                                         fontSize: 18,
+//                                         fontWeight: .medium,
+//                                         frameWidth: 140)
+                        Text("xxxxxxxxx")
+                        .foregroundColor(Color(.secondaryLabel))
+                        //.onTapGesture { vm.selectedCountry = country }
                     }
                 }
-                .task { await vm.getListaPaises() }
-                .alert("Error", isPresented: $vm.hasError, presenting: vm.state) { detail in Button("Retry", role: .destructive)
-                        { Task {await vm.getListaPaises()}}}
-                          message: { detail in if case let .failed(error) = detail { Text(error.localizedDescription)}}
             }
-            .navigationBarTitle("Country List", displayMode: .inline)
+            .task { await vm.getListaPaises() }
+            .alert("Error", isPresented: $vm.hasError, presenting: vm.state) { detail in Button("Retry", role: .destructive)
+                { Task {await vm.getListaPaises()}}}
+        message: { detail in if case let .failed(error) = detail { Text(error.localizedDescription)}}
         }
+        .navigationBarTitle("Country List", displayMode: .inline)
     }
 }
 
 
 
+    //                BarraBuscaView(searchText: $searchText, isSearching: $isSearching)
+    //                    .padding(.vertical, 5)
+    //                Divider()
+    //                    .frame(height: 4)
+    //                    .background(Color(.systemGray2))
+    //                    .padding(.bottom, 5)
+    //                Section(header:
+    //                        HStack
+    //                        {
+    //                            CabecalhoView(title: "País",
+    //                                          fontSize: 22,
+    //                                          fontWeight: .bold)
+    //                            .frame(width: 130, alignment: .leading)
+    //                            Spacer()
+    //                            CabecalhoView(title: "Casos",
+    //                                          fontSize: 22,
+    //                                          fontWeight: .bold)
+    //                            .frame(width: 90, alignment: .trailing)
+    //                            CabecalhoView(title: "Mortes",
+    //                                          fontSize: 22,
+    //                                          fontWeight: .bold)
+    //                            .frame(width: 90, alignment: .trailing)
+    //                        }
+    //                    )
+    //
