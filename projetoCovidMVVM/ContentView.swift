0e2d4479c9c49b9f79ec0d678ffb38a457fcb0ef
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-03-03 11:08:10 -0300

    ajuste no fundo da tabbar

--- projetoCovidMVVM/ContentView.swift
+++ projetoCovidMVVM/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View
 {
+
+    UITabBar.appearance().scrollEdgeAppearance = UITabBarAppearance.init(idiom: .unspecified)
     var body: some View
     {
         TabView
@@ -34,8 +36,17 @@ struct ContentView: View
                     Text("Gráficos")
                 })
         }
+        .onAppear {
+            let appearance = UITabBarAppearance()
+            appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+            appearance.backgroundColor = UIColor(Color.orange.opacity(0.2))
+            
+            // Use this appearance when scrolling behind the TabView:
+            UITabBar.appearance().standardAppearance = appearance
+            // Use this appearance when scrolled all the way up:
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
         .edgesIgnoringSafeArea(.top)
-        //.accentColor(Color.secondary)
     }
 }
 
