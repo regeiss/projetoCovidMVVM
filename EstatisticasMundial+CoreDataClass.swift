a1507d2246a87421b671894fd3cf0c7e18eb3154
Roberto <roberto.geiss.nh@icloud.com>
2023-04-26 19:34:12 -0300

    ajuste coredata

---
+++ EstatisticasMundial+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  EstatisticasMundial+CoreDataClass.swift
+//  projetoCovidMVVM
+//
+//  Created by Roberto Edgar Geiss on 26/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(EstatisticasMundial)
+public class EstatisticasMundial: NSManagedObject
+{
+
+}
---
+++ EstatisticasMundial+CoreDataProperties.swift
@@ -0,0 +1,35 @@
+//
+//  EstatisticasMundial+CoreDataProperties.swift
+//  projetoCovidMVVM
+//
+//  Created by Roberto Edgar Geiss on 26/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+extension EstatisticasMundial
+{
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<EstatisticasMundial>
+    {
+        return NSFetchRequest<EstatisticasMundial>(entityName: "EstatisticasMundial")
+    }
+
+    @NSManaged public var updatedDate: Date?
+    @NSManaged public var cases: Int32
+    @NSManaged public var deaths: Int32
+    @NSManaged public var recovered: Int32
+    @NSManaged public var todayCases: Int32
+    @NSManaged public var todayRecovered: Int32
+    @NSManaged public var active: Int32
+    @NSManaged public var critical: Int32
+    @NSManaged public var population: Int32
+
+}
+
+extension EstatisticasMundial : Identifiable
+{
+
+}
